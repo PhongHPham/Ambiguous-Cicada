9accc7c803e34b2745d23817d4e2a71d0092b9ae
Phong Pham <phizzy@gmail.com>
2015-10-08 20:54:37 -0700

    Edit config.js

--- server/config.js
+++ server/config.js
@@ -19,6 +19,12 @@ var config = {
   production: {
     dbpath: process.env.MONGOLAB_URI,
     port: process.env.PORT || 3000,
+    yelp: {
+      consumer_key: "xMbQVJGdx8gO-GoTwNzhoQ",
+      consumer_secret: "WlaUMIrmrPG_zq3eYHw-TUv1T2Q",
+      token: "Qcu_1iG7wN3wYIySD4pUMl_k-wuAEg2W",
+      token_secret: "O3GBw0ThCM08gWwTOMHaEMBVVvI"
+    },
     api_keys: {
       geocoding: process.env.APIKEY_GEOCODING,
     }
